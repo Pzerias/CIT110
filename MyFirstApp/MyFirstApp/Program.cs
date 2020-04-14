7ecd82107722c86dd1fac917f33a5aa49affed80
Pzerias <59912767+Pzerias@users.noreply.github.com>
2020-04-13 19:31:13 -0700

    Add files via upload

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user!");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
